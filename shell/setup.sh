731ba380c2e9b631673d12c9930555fae7844202
andsf <f.s.7272.f@gmail.com>
2016-03-08 13:53:20 +0900

    modify setup.sh

--- shell/setup.sh
+++ shell/setup.sh
@@ -13,3 +13,9 @@ fi
 ##NeoBundle install
 git clone git://github.com/Shougo/neobundle.vim
 
+#set symbolic link
+cd ~
+ln -s ~/dotfile/.vimrc ~/.vimrc
+ln -s ~/dotfile/.bashrc ~/.bashrc
+ln -s ~/dotfile/.git-prompt.sh ~/.git-prompt.sh
+ln -s ~/dotfile/.git-completion.bash ~/.git-completiom.bash
