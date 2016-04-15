cc0bcc6f04c7ae6e9dfaa6e4625662e3fd6dc80f
andsf <f.s.7272.f@gmail.com>
2016-04-15 17:27:41 +0900

    modify setup.sh

--- shell/setup.sh
+++ shell/setup.sh
@@ -5,11 +5,11 @@
 ##check directory and move directory
 echo 'check directory'
 if [ -e ~/.vim/bundle ]; then
- echo 'exit directory'
- cd ~/.vim/bundle
+  echo 'exit directory'
+  cd ~/.vim/bundle
 else
- mkdir -p ~/.vim/bundle
- cd ~/.vim/bundle
+  mkdir -p ~/.vim/bundle
+  cd ~/.vim/bundle
 fi
 
 ##NeoBundle install
@@ -19,6 +19,16 @@ git clone git://github.com/Shougo/neobundle.vim
 #set symbolic link
 echo 'set symbolic link'
 cd ~
+
+for file in ".vimrc" ".bashrc" ".git-prompt.sh" ".git-completion.bash"
+do
+	if [ -f ~/$file ]; then
+	  rm ~/$file
+	elif [ -L ~/$file ]; then
+	  unlink ~/$file
+	fi
+done
+
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
